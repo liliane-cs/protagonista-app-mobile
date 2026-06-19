fad32112c37c2a29e9eb8f4bac101217528e985d
liliane-cs <liliane.silva@residente.serratec.org>
2026-06-19 00:44:27 -0300

    pastas alteradas para compatibilidade com drawer e animated

--- src/routers/drawer.routes.tsx
+++ src/routers/drawer.routes.tsx
@@ -1,14 +1,15 @@
-
-import { View, Text } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DrawerParamList } from './navigation';
-import { TabsRoutes } from './tab.routes';
+// drawerRoutes.tsx
+import { View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DrawerParamList } from "./navigation";
+import { TabsRoutes } from "./tab.routes";
+//import { MeuPerfil } from "../pages/MeuPerfil";
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 function PlaceholderScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Tela em construção</Text>
     </View>
   );
@@ -20,23 +21,23 @@ export const DrawerRoutes = () => {
       <Drawer.Screen
         name="TabsRoutes"
         component={TabsRoutes}
-        options={{ drawerLabel: 'Início' }}
+        options={{ drawerLabel: "Início" }}
       />
       <Drawer.Screen
         name="Profissionais"
         component={PlaceholderScreen}
-        options={{ drawerLabel: 'Profissionais' }}
+        options={{ drawerLabel: "Profissionais" }}
       />
       <Drawer.Screen
         name="Favoritos"
         component={PlaceholderScreen}
-        options={{ drawerLabel: 'Favoritos' }}
+        options={{ drawerLabel: "Favoritos" }}
       />
-      <Drawer.Screen
+      {/* <Drawer.Screen
         name="MeuPerfil"
-        component={PlaceholderScreen}
-        options={{ drawerLabel: 'Meu Perfil' }}
-      />
+        component={MeuPerfil}
+        options={{ drawerLabel: "Meu Perfil" }}
+      /> */}
     </Drawer.Navigator>
   );
 };
